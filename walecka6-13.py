0719ed5cbb1ddd1302da8e306280ab68fbf0b9c7
Emma Rice <er512317@ohio.edu>
2020-02-28 15:57:29 -0500

    subplots fixed

--- walecka6-13.py
+++ walecka6-13.py
@@ -20,15 +20,26 @@ def my_range(start, end, step):
 
 QR = []
 Flist = []
+Flistabs = []
 for qR in my_range(0.00001, 20.00001,0.0001):
 	QR.append(qR)
 	Flist.append(F(qR))
+	Flistabs.append(abs(F(qR)))
 
-fig, ax = plt.subplots(1,1)
-ax.plot(QR,Flist, 'r--',label='form factor of uniformly charged sphere') 
-ax.set_xlim(0,20)
-ax.set_ylim(-.2,2)
-ax.set_xlabel('qR')
-ax.set_ylabel('F')
-ax.set_yscale('log')
-fig.savefig('walecka6-13.png',format='PNG') 
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(QR,Flist, 'r--') #row=0, col=0
+ax[0].set_xlim(0,20)
+ax[0].set_ylim(-.2,1.1)
+ax[0].set_xlabel('qR')
+ax[0].set_ylabel('F')
+ax[0].set_yscale('linear')
+ax[0].set_title('Linear Form Factor')
+ax[1].plot(QR,Flistabs, 'r--') #row=0, col=1
+ax[1].set_xlim(0,20)
+ax[1].set_ylim(0.0001,2)
+ax[1].set_xlabel('qR')
+ax[1].set_ylabel('F')
+ax[1].set_yscale('log')
+ax[1].set_title('Log Form Factor')
+fig.savefig('walecka6-13.png',format='PNG')
+fig.show()
