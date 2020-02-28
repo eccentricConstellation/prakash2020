118f59dbefed0991cfbba76bf9eff48c67ec45d5
Emma Rice <er512317@ohio.edu>
2020-02-28 10:36:42 -0500

    need to work on log scale plot

--- walecka6-13.py
+++ walecka6-13.py
@@ -20,14 +20,15 @@ def my_range(start, end, step):
 
 QR = []
 Flist = []
-for qR in my_range(0.00001, 20.00001,0.00001):
+for qR in my_range(0.00001, 20.00001,0.0001):
 	QR.append(qR)
 	Flist.append(F(qR))
 
-fig1 = plt.figure()
-plt.plot(QR,Flist, 'r--') 
-plt.title('form factor of uniformly charged sphere')
-plt.axis([0, 20, -.2, 1.1])
-plt.xlabel('qR')
-plt.ylabel('F')
-plt.show() 
+fig, ax = plt.subplots(1,1)
+ax.plot(QR,Flist, 'r--',label='form factor of uniformly charged sphere') 
+ax.set_xlim(0,20)
+ax.set_ylim(-.2,2)
+ax.set_xlabel('qR')
+ax.set_ylabel('F')
+ax.set_yscale('log')
+fig.savefig('walecka6-13.png',format='PNG') 
