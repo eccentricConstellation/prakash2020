1c471e50f7c5a136877b9093f439d1b459566b0f
Emma Rice <er512317@ohio.edu>
2020-02-28 19:20:37 -0500

    better graphs

--- walecka6-13.py
+++ walecka6-13.py
@@ -21,7 +21,7 @@ def my_range(start, end, step):
 QR = []
 Flist = []
 Flistabs = []
-for qR in my_range(0.00001, 20.00001,0.0001):
+for qR in my_range(0.00001, 20.00001,0.000001):
 	QR.append(qR)
 	Flist.append(F(qR))
 	Flistabs.append(abs(F(qR)))
@@ -36,7 +36,7 @@ ax[0].set_yscale('linear')
 ax[0].set_title('Linear Form Factor')
 ax[1].plot(QR,Flistabs, 'r--') #row=0, col=1
 ax[1].set_xlim(0,20)
-ax[1].set_ylim(0.0001,2)
+ax[1].set_ylim(0.000001,2)
 ax[1].set_xlabel('qR')
 ax[1].set_ylabel('F')
 ax[1].set_yscale('log')
